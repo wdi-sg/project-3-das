664335e68a9035ff4f11214d5e1afe08d04c7679
andre the <andre.the12@gmail.com>
2017-03-13 11:35:44 +0800

    add on more fields in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170312072549) do
+ActiveRecord::Schema.define(version: 20170313023606) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -75,6 +75,9 @@ ActiveRecord::Schema.define(version: 20170312072549) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "condo"
+    t.string   "unit"
+    t.integer  "contact"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
