4fcc5174051be99e0ac2b317f77b88e3f3bc0d58
andre the <andre.the12@gmail.com>
2017-03-14 22:33:15 +0800

    added completed col

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314091844) do
+ActiveRecord::Schema.define(version: 20170314143103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170314091844) do
     t.datetime "updated_at", null: false
     t.datetime "date"
     t.datetime "time"
+    t.boolean  "completed"
     t.index ["service_id"], name: "index_requests_on_service_id", using: :btree
     t.index ["user_id"], name: "index_requests_on_user_id", using: :btree
   end
@@ -92,4 +93,7 @@ ActiveRecord::Schema.define(version: 20170314091844) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "invoices", "requests"
+  add_foreign_key "requests", "services"
+  add_foreign_key "requests", "users"
 end
