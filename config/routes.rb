92a760f0e00bfb8ab14a5dce54a37defb3893b4f
Heng Desmond <desmond@dsrecruitment.sg>
2017-03-16 14:30:57 +0800

    added about routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'static#homepage'
 
+get "/about" => "static#about"
+
+
 devise_for :users, :controllers => { registrations: 'users/registrations' }
 
 end
