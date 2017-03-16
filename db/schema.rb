7c71aa0e423a9c2c16e3cb33c016cee6f08a63a9
Heng Desmond <desmond@dsrecruitment.sg>
2017-03-16 10:16:32 +0800

    facebook authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314143103) do
+ActiveRecord::Schema.define(version: 20170316020213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 20170314143103) do
     t.integer  "postal_code"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "facebookusers", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "provider_id"
+    t.string   "provider_hash"
+    t.string   "email"
+    t.string   "name"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "invoices", force: :cascade do |t|
