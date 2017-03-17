ce8f69c6bd24ea957cd5e7a56ab5d1cf2fd52786
andre the <andre.the12@gmail.com>
2017-03-17 10:44:47 +0800

    added test for services

--- test/models/service_test.rb
+++ test/models/service_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ServiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @laundry = services(:laundry)
+  end
+
+  test "name of service must be laundry" do
+    assert_equal 'Laundry', @laundry.kind, 'The type of service does not exist'
+  end
+
+  test "service price should be one" do
+    assert_equal 1, @laundry.cost, 'This is the wrong price'
+  end
+
+  test "service should have a description" do
+    assert_equal "laundry is fun", @laundry.description, 'No description'
+  end
 end
